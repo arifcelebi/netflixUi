46b69ada6526292df9fe2158ba4fe82c4aed0db3
arifcelebi <61031852+arifcelebi@users.noreply.github.com>
2022-08-24 17:47:27 +0300

    Initial Commit

---
+++ hw3/Fonts.swift
@@ -0,0 +1,29 @@
+//
+//  Fonts.swift
+//  hw3
+//
+//  Created by Arif Celebi on 17.08.2022.
+//
+
+import UIKit
+
+class Fonts: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ hw3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw3
+//
+//  Created by Arif Celebi on 14.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
